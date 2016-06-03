c45b818ff75d5508ba22873984b0b0bbe2a2e757
Brian Park <brianpark25@gmail.com>
2016-06-03 11:56:19 -0700

    Push notification fixes

--- app/controllers/concerns/notifications.rb
+++ app/controllers/concerns/notifications.rb
@@ -10,7 +10,7 @@ module Notifications
         if app.save
           notification = app.notifications.build(
             destinations: tokens,
-            data: { title: title, message: message }
+            data: { message: { title: title, body: message } }
           )
 
           if notification.save
